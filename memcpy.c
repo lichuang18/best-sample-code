fb418de8b637d56c33d1a395d3349e08c5a360bd
lichuang <983459843@qq.com>
2023-03-14 10:44:03 +0800

    lichuang

--- memcpy.c
+++ memcpy.c
@@ -9,30 +9,48 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define N 1024
+
+#define N 1024//128M
+#define S 128*1024*1024  //128M
 int main(){
-    int a[N];
-    int b[N];
+    int *a;
+    int ret;
+    //int *a=(int*)malloc(2)
+    ret = posix_memalign(&a,4096,S);
+    if(ret)
+    {
+        printf ("mem error\n");
+        return -1;
+    } 
+    int ret2;
+    int *b;
+    ret2 = posix_memalign(&b,4096,S);
+    if(ret2)
+    {
+        printf ("mem error\n");
+        return -1;
+    } 
+
+    memset(a,0,S);
+    memset(b,0,S);
     int i, size = sizeof(int);
-    //printf("size of int :%ld, total size : %d\n",sizeof(int),N*size);
-    /*
-    int fd;
-    if( (fd=open("/dev/sdf", O_RDWR)) < 0 ){  //以二进制方式打开 |O_DIRECT|O_SYNC
-        exit(0);
-    }
-*/
     struct  timeval  start1;
 	struct  timeval  end1;
     unsigned long timer1;
-
-    gettimeofday(&start1, NULL);
-    for(int i=0;i<1000;i++)
+    int sum=0;
+    for(int j=0;j<20;j++)//每次4M，下一次从下一个4M开始  共80M
     {
-        memcpy(b, a, size * N);
+        gettimeofday(&start1, NULL);
+        for(int i=0;i<1000;i++)//每次4K，下次从下一个4k开始访问  共4M
+        {
+            memcpy(b+j*1024*1024+i*1024, a+j*1024*1024+i*1024, size * N); //4K粒度cpy  
+        }
+        gettimeofday(&end1, NULL);
+        int speedtime = (end1.tv_sec * 1000000 + end1.tv_usec) - (start1.tv_sec * 1000000 + start1.tv_usec);
+        printf("%dth exec time :%d us\n",j,speedtime);
+        sum=sum+speedtime;
     }
-    gettimeofday(&end1, NULL);
-    int speedtime = (end1.tv_sec * 1000000 + end1.tv_usec) - (start1.tv_sec * 1000000 + start1.tv_usec);
-    printf("exec time :%d us\n",speedtime);
+    printf("avg: exec time :%d us\n",sum/20);
    //fsync(fd);
     //close(fd);
     return 0;
